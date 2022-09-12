304f80e7523d5e512e41d8ee74b21e18718e82da
StephenTurk90 <stephenjturk@gmail.com>
2022-09-12 18:33:02 -0400

    encountered an error when changing snippets/admin.py when attempting the highlighted field to readonly_fields

--- snippets/admin.py
+++ snippets/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from .models import Snippet
 
+class SnippetAdmin(admin.ModelAdmin):
+    readonly_fields = ('highlighted',)
 
-admin.site.register(Snippet)
+admin.site.register(Snippet, SnippetAdmin)
